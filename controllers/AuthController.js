d5095c0d2d77c41bba295f1b409d0d9b51aeaf45
Sark_oo7 <kosinick01@gmail.com>
2024-05-15 10:21:55 +0100

    task 2 - 4

---
+++ controllers/AuthController.js
@@ -0,0 +1,44 @@
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  static async getConnect(request, response) {
+    const authData = request.header('Authorization');
+    let userData = authData.split(' ')[1];
+    const buff = Buffer.from(userData, 'base64');
+    userData = buff.toString('ascii');
+    userData = userData.split(':');
+    if (userData.length !== 2) {
+      response.status(401).json({ error: 'Unauthorized1' });
+      return;
+    }
+
+    const hashedpassword = sha1(userData[1]);
+    const users = dbClient.db.collection('users');
+    users.findOne({ email: userData[0], password: hashedpassword }, async (err, user) => {
+      if (user) {
+        const token = uuidv4();
+        const key = `auth_${token}`;
+        await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+        response.status(200).json({ token });
+      } else {
+        response.status(401).json({ error: 'Unauthorized' });
+      }
+    });
+  }
+
+  static async getDisconnect(request, response) {
+    const authToken = request.header('X-Token');
+    const key = `auth_${authToken}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      await redisClient.del(key);
+      response.status(204).json({});
+    } else {
+      response.status(401).json({ error: 'Unauthorized' });
+    }
+  }
+}
+module.exports = AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
-import { Router } from "express";
-import AppController from "../controllers/AppController";
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/Stats', AppController.getStats);
-
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
